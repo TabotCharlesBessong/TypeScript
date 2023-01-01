775b79f78584b98be743e254e112e8e94d636481
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-01 10:47:06 +0100

    function return

--- functions/intro.js
+++ functions/intro.js
@@ -1,7 +1,9 @@
 "use strict";
 exports.__esModule = true;
+// precising what a function should return 
 var addTwo = function (num) {
     return num + 2;
+    // return "hello"
 };
 console.log(addTwo(7));
 var getUpper = function (val) {
@@ -15,3 +17,19 @@ var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = true; }
 };
 loginUser('CJ', 'c@.com');
+// how to return more than one type 
+// const getValue = (myVal:number) => {
+//   if(myVal > 5) return true
+//   return "200 OK"
+// }
+var heros = ["Superman", "Aquaman", "Flash", "Batman"];
+// const heros = [1,2,3,4]
+heros.map(function (hero) {
+    return "my hero is ".concat(hero);
+});
+var consoleError = function (errMsg) {
+    console.log(errMsg);
+};
+var handleError = function (errMsg) {
+    throw new Error(errMsg);
+};
--- functions/intro.ts
+++ functions/intro.ts
@@ -1,6 +1,8 @@
 
-const addTwo = (num:number) => {
+// precising what a function should return 
+const addTwo = (num:number): number => {
   return num + 2
+  // return "hello"
 }
 
 console.log(addTwo(7))
@@ -19,5 +21,26 @@ console.log(getUpper('i love you'))
 const loginUser = (name:string,email:string,isPaid:boolean = true) => {}
 
 loginUser('CJ','c@.com')
+
+// how to return more than one type 
+// const getValue = (myVal:number) => {
+//   if(myVal > 5) return true
+//   return "200 OK"
+// }
+
+const heros = ["Superman","Aquaman","Flash","Batman"]
+// const heros = [1,2,3,4]
+
+heros.map((hero):string =>  {
+  return `my hero is ${hero}`
+})
+
+const consoleError = (errMsg:string):void => {
+  console.log(errMsg)
+}
+
+const handleError = (errMsg:string):never => {
+  throw new Error(errMsg)
+}
 
 export {}
