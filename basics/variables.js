92405569c265d8cb43614fb35120498e70306280
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-01 10:09:34 +0100

    primitive variables

---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var greetings = 'Charles';
+// greetings.toLoweCase() 
+console.log(greetings);
+// numbers 
+var userId = 35892432;
+// userId.
+// Boolean 
+var isLoggedIn = false;
+// any 
+var hero;
+var getHero = function () {
+    return "Superman";
+};
+hero = getHero();
+console.log(hero);
---
+++ basics/variables.ts
@@ -0,0 +1,24 @@
+
+let greetings: string = 'Charles'
+// greetings.toLoweCase() 
+console.log(greetings);
+
+
+// numbers 
+let userId = 35892432
+// userId.
+
+// Boolean 
+let isLoggedIn: boolean = false
+
+// any 
+let hero:string;
+
+const getHero = ()=>{
+  return "Superman"
+}
+
+hero = getHero()
+console.log(hero)
+
+export {}
