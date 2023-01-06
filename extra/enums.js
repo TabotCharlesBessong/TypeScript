fc2008ce3f5fd5b2163d41939301d084bfd2aa2e
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-06 17:12:55 +0100

    enums in ts

---
+++ extra/enums.js
@@ -0,0 +1,10 @@
+var hcSeat = "aisle" /* SeatChoice.AISLE */;
+
+// var SeatChoice;
+// (function (SeatChoice) {
+// 	SeatChoice[(SeatChoice["AISLE"] = 0)] = "AISLE";
+// 	SeatChoice[(SeatChoice["MIDDLE"] = 1)] = "MIDDLE";
+// 	SeatChoice[(SeatChoice["WINDOW"] = 2)] = "WINDOW";
+// 	SeatChoice[(SeatChoice["FOURTH"] = 3)] = "FOURTH";
+// })(SeatChoice || (SeatChoice = {}));
+// var SeatChoice = SeatChoice.AISLE;
---
+++ extra/enums.ts
@@ -0,0 +1,9 @@
+
+const enum SeatChoice {
+  AISLE = 'aisle',
+  MIDDLE = 1,
+  WINDOW,
+  FOURTH
+}
+
+const hcSeat = SeatChoice.AISLE
