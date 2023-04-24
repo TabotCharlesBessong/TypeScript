190fd7223831cbd1f57cc14204bbdd659d465f57
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-04-24 08:32:55 +0100

    getters and setters

--- typescript_scr/dist/index.js
+++ typescript_scr/dist/index.js
@@ -1,9 +1,35 @@
 "use strict";
+// class User {
+//   public email:string
+//   private name:string
+//   private readonly city: string = 'Limbe'
+//   constructor(email:string,name:string){
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
 class User {
     constructor(email, name) {
-        this.city = 'Limbe';
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
+        this.city = "Limbe";
+    }
+    deleteToken() {
+        console.log('Token deleted');
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error('Course count must be greater than 1');
+        }
+        this._courseCount = courseNum;
     }
 }
-const charles = new User('charles@tabot.com', 'charles');
+const charles = new User("charles@tabot.com", "charles");
+console.log(charles);
--- typescript_scr/src/index.ts
+++ typescript_scr/src/index.ts
@@ -1,12 +1,44 @@
-
+// class User {
+//   public email:string
+//   private name:string
+//   private readonly city: string = 'Limbe'
+//   constructor(email:string,name:string){
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
 class User {
-  email:string
-  name:string
-  private readonly city: string = 'Limbe'
-  constructor(email:string,name:string){
-    this.email = email;
-    this.name = name;
+
+  private _courseCount = 1
+
+  readonly city: string = "Limbe";
+  constructor(
+    public email: string, 
+    public name: string,
+    // private userId: string
+  ) {
+  }
+
+  private deleteToken() {
+    console.log('Token deleted')
+  }
+
+  get getAppleEmail(): string{
+    return `apple${this.email}`
+  }
+
+  get courseCount(): number {
+    return this._courseCount
+  }
+
+  set courseCount(courseNum){
+    if(courseNum <=1) {
+      throw new Error('Course count must be greater than 1')
+    }
+    this._courseCount = courseNum
   }
 }
 
-const charles = new User('charles@tabot.com','charles')
+const charles = new User("charles@tabot.com", "charles");
+
+console.log(charles);
