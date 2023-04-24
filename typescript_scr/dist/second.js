97e79c63ed65484aab57c54ad1f1bae1ce908b2d
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-04-24 08:50:05 +0100

    interface in classes

---
+++ typescript_scr/dist/second.js
@@ -0,0 +1,19 @@
+"use strict";
+class Instagram {
+    constructor(cameraMode, filter, burst) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+}
+class Youtube {
+    constructor(cameraMode, filter, burst, shorts) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+        this.shorts = shorts;
+    }
+    createStory() {
+        console.log('story created');
+    }
+}
