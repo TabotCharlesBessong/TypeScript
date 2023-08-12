dd2b6ebb1cf293691664d6b25a7d1e5ede5a21d7
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-08-12 21:07:43 +0100

    feat:addijng marker to the map

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,9 +1,31 @@
+import { Company } from './Company';
+import { User } from './User';
 export class CustomMap {
   private googleMap: google.maps.Map;
   constructor(divId:string) {
     this.googleMap = new google.maps.Map(document.getElementById(divId)!, {
       center: { lat: 37.422, lng: -122.084 },
       zoom: 1,
+    });
+  }
+
+  public addUserMarker(user:User): void {
+    const marker = new google.maps.Marker({
+      position:{
+        lat: user.location.lat,
+        lng: user.location.lng
+      },
+      map: this.googleMap,
+    });
+  }
+
+  public addCompanyMarker(company:Company): void {
+    const marker = new google.maps.Marker({
+      position:{
+        lat: company.location.lat,
+        lng: company.location.lng
+      },
+      map: this.googleMap,
     });
   }
 }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,6 +1,11 @@
 /// <reference types="@types/google.maps" />
-// import { User } from './User';
-// import { Company } from './Company';
+import { User } from './User';
+import { Company } from './Company';
 import { CustomMap } from "./CustomMap";
 
 const customMap = new CustomMap('map');
+const user = new User()
+const company = new Company()
+
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
