feb3e3eb8a610d4ef2b1dfe1fd4c28e6d2a235ef
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-08-22 12:57:54 +0100

    destructuring annotations

--- functions/annotations/annotations/functions.js
+++ functions/annotations/annotations/functions.js
@@ -10,3 +10,21 @@ function divide(a, b) {
 var multiply = function (a, b) {
     return a * b;
 };
+var logger = function (message) {
+    console.log(message);
+};
+var throwError = function (message) {
+    if (!message) {
+        throw new Error(message);
+    }
+};
+var todaysWeather = {
+    date: new Date(),
+    weather: "sunny",
+};
+var logWeather = function (_a) {
+    var date = _a.date, weather = _a.weather;
+    console.log(date);
+    console.log(weather);
+};
+logWeather(todaysWeather);
